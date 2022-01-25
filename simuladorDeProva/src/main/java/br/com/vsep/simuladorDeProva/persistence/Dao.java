eefc5204a1d57b187ade78834cc6728048200016
Vitória da Silva Eleutério Pinto <60067591+vitoriasilva98@users.noreply.github.com>
2022-01-25 20:30:05 -0300

    Atualização de credenciais persistence

--- simuladorDeProva/src/main/java/br/com/vsep/simuladorDeProva/persistence/Dao.java
+++ simuladorDeProva/src/main/java/br/com/vsep/simuladorDeProva/persistence/Dao.java
@@ -22,12 +22,10 @@ public class Dao {
 	public void open() throws Exception {
 		Class.forName("org.postgresql.Driver");
 //		con = DriverManager.getConnection(
-//		"jdbc:postgresql://ec2-3-216-113-109.compute-1.amazonaws.com:5432/ddo2218lf7a6ov?user=ujhtxvkkftzjcv&password=7e44792a0214f21bc56adb2085ff69b1def6a08f8a387e26686c7ee586d04078");
+//		"jdbc:postgresql://<host>:5432/<database>?user=<user>&password=<password>");
 		
-//		con = DriverManager.getConnection(
-//				"jdbc:postgresql://ec2-3-95-130-249.compute-1.amazonaws.com:5432/d74ibod5guakhu?user=bdgqgglkhyrgjl&password=009cf0a97680e3937ef52599a364159144714bd3adcd35bce5514d4200dc13f4");
-
-		con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/simuladordeprova", "postgres", "stefanini123");
+//	Conexão remota
+//		con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/simuladordeprova", "postgres", "senha");
 	}
 
 	public void close() throws Exception {
